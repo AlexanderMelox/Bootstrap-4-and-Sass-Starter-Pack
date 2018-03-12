5a39dd876f5d4dc6e5526baca02955abf201822a
Alexander Melo <alexandermelox@gmail.com>
2018-03-11 21:52:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bootstrap 4 and Sass Starter Pack
+
+### Whats included?
+
+- Gulp tasks
+- Sass Compiler
+- BrowserSync
+- Bootstrap
+- jQuery and Popper.js
+
+### How to start dev server?
+
+1. Download Project.
+2. In root folder run `npm install`
+3. To run gulp task run `npm start`
+4. Open browser to `localhost:3000`
+5. Start Developing!
+
+Contact me: Alexander Melo 🙃 [alexandermelo.com](https://github.com/AlexanderMelox/Bootstrap-4-Starter/blob/master/alexandermelo.com)
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Compile Sass
+
+gulp.task('sass', () => {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+});
+
+// Watch & serve
+gulp.task('serve', ['sass'], () => {
+    browserSync.init({
+        server: './src'
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/*.html']).on('change', browserSync.reload);
+});
+
+// Default
+gulp.task('default', ['serve']);
